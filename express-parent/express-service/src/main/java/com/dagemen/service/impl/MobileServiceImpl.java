626da3b311499e6c4d06d3d3035dd985b6d96e6c
wangtongkang <513190048@qq.com>
2017-08-20 13:19:44 +0800

    保存快递单

--- express-parent/express-service/src/main/java/com/dagemen/service/impl/MobileServiceImpl.java
+++ express-parent/express-service/src/main/java/com/dagemen/service/impl/MobileServiceImpl.java
@@ -1,10 +1,13 @@
 package com.dagemen.service.impl;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.dagemen.dto.ExpressDTO;
+import com.dagemen.entity.Express;
 import com.dagemen.entity.User;
 import com.dagemen.service.ExpressService;
 import com.dagemen.service.MobileService;
 import com.dagemen.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,16 +26,46 @@ public class MobileServiceImpl implements MobileService {
     @Override
     public boolean insertExpress(ExpressDTO expressDTO) {
 
-        User reciver = new User();
-        reciver.setAddress(expressDTO.getReceiverAddress());
-        reciver.setName(expressDTO.getReceiverName());
-        reciver.setPhone(expressDTO.getReceiverPhone());
-        reciver.setProvinceId(expressDTO.getReceiverProvinceId());
-        reciver.setCityId(expressDTO.getReceiverCityId());
-        reciver.setDistrictId(expressDTO.getReceiverDistrictId());
-        reciver.setAddress(expressDTO.getReceiverAddress());
+        Express express = new Express();
+        BeanUtils.copyProperties(expressDTO, express);
 
-        return false;
+        User sender = new User();
+        sender.setName(express.getSenderName());
+        sender.setPhone(express.getSederPhone());
+
+        sender.setProvinceId(express.getSenderProvinceId());
+        sender.setCityId(express.getSenderCityId());
+        sender.setDistrictId(express.getReceiverDistrictId());
+        sender.setAddress(express.getSenderAddress());
+        User oldSender = getUser(sender.getPhone());
+        if (oldSender == null) {
+            sender.setPhone(oldSender.getPhone());
+        }
+        userService.insertOrUpdate(sender);
+        express.setSenderId(sender.getId());
+
+        User receiver = new User();
+        receiver.setAddress(expressDTO.getReceiverAddress());
+        receiver.setName(expressDTO.getReceiverName());
+        receiver.setPhone(expressDTO.getReceiverPhone());
+        receiver.setProvinceId(expressDTO.getReceiverProvinceId());
+        receiver.setCityId(expressDTO.getReceiverCityId());
+        receiver.setDistrictId(expressDTO.getReceiverDistrictId());
+        receiver.setAddress(expressDTO.getReceiverAddress());
+        User oldReceiver = getUser(receiver.getPhone());
+        if (oldReceiver == null) {
+            receiver.setId(oldReceiver.getId());
+        }
+        userService.insertOrUpdate(receiver);
+        express.setReceiverId(receiver.getId());
+
+        return expressService.insertOrUpdate(express);
+    }
+
+    private User getUser(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        return userService.selectOne(new EntityWrapper<>(user));
     }
 
 
