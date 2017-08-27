d07f8579279bb3f4715b5ad1d3dc629eea260db5
apple <513190048@qq.com>
2017-08-27 18:06:36 +0800

    前台页面提交功能

--- express-parent/express-web/src/main/webapp/service.js
+++ express-parent/express-web/src/main/webapp/service.js
@@ -41,7 +41,7 @@ Service.prototype = {
     },
 
     //post
-    postAll: function (param, callback, errorCallBack) {
+    post: function (param, callback, errorCallBack) {
         var error = this._error_handler;
         $.ajax({
             cache: false,
@@ -55,8 +55,7 @@ Service.prototype = {
                 if (response.success) {
                     callback(response);
                 } else {
-                    mini.alert(response.msg);
-                    btnOkEnabled(true);
+                    alert(response.msg);
                 }
             },
             error: function (jqXHR) {
@@ -119,6 +118,19 @@ Service.prototype = {
             },
             error: this._error_handler
         });
+    },
+    getUrlParams:function(){
+        var args=new Object();
+        var query=location.search.substring(1);//获取查询串
+        var pairs=query.split("&");//在逗号处断开
+        for(var   i=0;i<pairs.length;i++) {
+            var pos=pairs[i].indexOf('=');//查找name=value
+            if(pos==-1)   continue;//如果没有找到就跳过
+            var argname=pairs[i].substring(0,pos);//提取name
+            var value=pairs[i].substring(pos+1);//提取value
+            args[argname]=unescape(value);//存为属性
+        }
+        return args;
     }
 };
 
