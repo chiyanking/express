1b668a21b3de57222ed514deeff36f5e4740b713
wangtk <wangtk@yangche51.com>
2017-10-13 01:12:08 +0800

    错误测试类提交

--- express-parent/express-dao/src/test/java/com/dagemen/AppTest.java
+++ express-parent/express-dao/src/test/java/com/dagemen/AppTest.java
@@ -1,15 +1,10 @@
 package com.dagemen;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest {
     /**
      * Create the test case
      *
@@ -17,21 +12,15 @@ public class AppTest
      */
     public AppTest( String testName )
     {
-        super( testName );
+
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp() {
-        assertTrue( true );
-    }
+
 }
