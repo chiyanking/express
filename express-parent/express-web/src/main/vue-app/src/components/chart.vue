344acb838ba56f7f7ebe3c4566b082822db97d2e
wangtk <wangtk@yangche51.com>
2017-10-13 23:49:54 +0800

    按需加载

--- express-parent/express-web/src/main/vue-app/src/components/chart.vue
+++ express-parent/express-web/src/main/vue-app/src/components/chart.vue
@@ -4,7 +4,13 @@
   </div>
 </template>
 <script>
-  import echarts from 'echarts';
+  // 引入 ECharts 主模块
+  var echarts = require('echarts/lib/echarts');
+  // 引入柱状图
+  require('echarts/lib/chart/bar');
+  // 引入提示框和标题组件
+  require('echarts/lib/component/tooltip');
+  require('echarts/lib/component/title');
 
   export default {
     data: function () {
@@ -13,18 +19,12 @@
     mounted() {
       // 基于准备好的dom，初始化echarts实例
       var myChart = echarts.init(document.getElementById('echart-container'));
-
-      // 指定图表的配置项和数据
-      var option = {
-        title: {
-          text: 'ECharts 入门示例'
-        },
+      // 绘制图表
+      myChart.setOption({
+        title: { text: 'ECharts 入门示例' },
         tooltip: {},
-        legend: {
-          data: ['销量']
-        },
         xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
         },
         yAxis: {},
         series: [{
@@ -32,13 +32,9 @@
           type: 'bar',
           data: [5, 20, 36, 10, 10, 20]
         }]
-      };
-
-      // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option);
+      });
     }
   }
-
 </script>
 <style>
   .echarts {
