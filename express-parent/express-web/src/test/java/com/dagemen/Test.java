4b0a3f306e2a1fbdda77dd51d83d697e612ebec5
wangtk <wangtk@yangche51.com>
2017-10-21 01:00:36 +0800

    pecs原则 学习笔记

--- express-parent/express-web/src/test/java/com/dagemen/Test.java
+++ express-parent/express-web/src/test/java/com/dagemen/Test.java
@@ -4,7 +4,28 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/app*.xml","classpath:spring-mvc/spring-*.xml"})
+@ContextConfiguration({"classpath:spring/app*.xml", "classpath:spring-mvc/spring-*.xml"})
 public class Test {
+    public static void main(String[] args) {
+        List<? extends Number> list1 = Arrays.asList(1, 2, 3, 5, 6);
+//        list1.add(new Integer(1));
+        for (Number number : list1) {
+            System.out.println(number);
+        }
+
+        System.out.println("---------------分割线----------------");
+        List<? super Number> list2 = new ArrayList<>();
+        list2.add(new Integer(1));
+        list2.add(new Long(1l));
+        list2.add(new Float(1f));
+
+        for (Object o : list2) {
+
+            System.out.println(o);
+        }
+
+    }
 }
